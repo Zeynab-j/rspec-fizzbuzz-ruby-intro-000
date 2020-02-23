e6a161c9dda92dbd5dbbc8959368480c17047157
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-23 01:43:31 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,4 @@
 require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
 
-
-def "fizzbuzz"
-end
+require_relative '../fizzbuzz.rb'
